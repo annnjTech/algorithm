15896faa7e9d7761bc68c181cd5c5ebee0a2cd7c
annnj <380904787@qq.com>
2024-12-19 15:07:48 +0800

    class05的使用位运算设计的整数+-*/计算器

--- class05/cmd/bit_calculator/main.go
+++ class05/cmd/bit_calculator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"algorithm/class05/bit_calculator"
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -10,5 +11,10 @@ func main() {
 	fmt.Println(bit_calculator.NegNum(-46))
 	fmt.Println(bit_calculator.Minus(2, 1))
 	fmt.Println(bit_calculator.Multi(8, 3))
-	fmt.Println(bit_calculator.Divide(10, 2))
+	fmt.Println(bit_calculator.Divide(math.MinInt64, math.MinInt64))
+	fmt.Println(bit_calculator.Divide(1, math.MinInt64))
+	fmt.Println(bit_calculator.Divide(math.MinInt64, -1))
+	fmt.Println(math.MaxInt64)
+	fmt.Println(bit_calculator.Divide(math.MinInt64, 100))
+	fmt.Println(bit_calculator.Divide(10, 3))
 }
