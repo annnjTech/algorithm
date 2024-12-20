e23a9409ea0bf6f68f8f3680321efe0cd9513b60
annnj <380904787@qq.com>
2024-12-20 08:50:41 +0800

    class06的根据先序结果和中序结果建出整棵二叉树的方法2

--- class06/construct_binary_tree/construct_binary_tree.go
+++ class06/construct_binary_tree/construct_binary_tree.go
@@ -26,3 +26,29 @@ func f(pre []int, l1, r1 int, in []int, l2, r2 int) *model.TreeNode {
 	head.Right = f(pre, l1+findIndex-l2+1, r1, in, findIndex+1, r2)
 	return head
 }
+
+func BuildTreeFromPreorderInorder2(pre, in []int) *model.TreeNode {
+	if pre == nil || in == nil || len(pre) != len(in) {
+		return nil
+	}
+	hashMap := make(map[int]int)
+	for key, val := range in {
+		hashMap[val] = key
+	}
+	return g(pre, 0, len(pre)-1, in, 0, len(in)-1, hashMap)
+}
+
+func g(pre []int, l1, r1 int, in []int, l2, r2 int, hashMap map[int]int) *model.TreeNode {
+	if l1 > r1 {
+		return nil
+	}
+
+	head := &model.TreeNode{Value: pre[l1]}
+	if l1 == r1 {
+		return head
+	}
+	findIndex := hashMap[pre[l1]]
+	head.Left = g(pre, l1+1, l1+findIndex-l2, in, l2, findIndex-1, hashMap)
+	head.Right = g(pre, l1+findIndex-l2+1, r1, in, findIndex+1, r2, hashMap)
+	return head
+}
